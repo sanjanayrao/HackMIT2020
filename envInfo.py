a25f10b71288ead02120c9fe57fb15baafe7cc47
Matt Wildman <mjwildman15@gmail.com>
2020-09-19 13:05:26 -0500

    Cleanup

--- envInfo.py
+++ envInfo.py
@@ -7,6 +7,31 @@ import requests
 # import folium
 
 
+def parse_json(json_loaded):
+
+    baqi = json_loaded['data']['indexes']['baqi']
+    baqi.pop('display_name')
+    health = json_loaded['data']['health_recommendations']['general_population']
+
+    pols = json_loaded['data']['pollutants']
+    pols_dict = {}
+    for item in pols:
+        new_dict = {}
+        new_dict['display_name'] = pols[item]['display_name']
+        new_dict['full_name'] = pols[item]['full_name']
+        new_dict['aqi'] = pols[item]['aqi_information']['baqi']['aqi']
+        new_dict['color'] = pols[item]['aqi_information']['baqi']['color']
+        new_dict['effects'] = pols[item]['sources_and_effects']['effects']
+        new_dict['sources'] = pols[item]['sources_and_effects']['sources']
+        pols_dict[item] = new_dict
+
+    result = {}
+    result['baqi'] = baqi
+    result['health'] = health
+    result['pollutants'] = pols_dict
+    return result
+
+
 def get_air_quality(lat, lon):
     api_url = 'https://api.breezometer.com/air-quality/v2/current-conditions?lat={}&lon={}&key=bf934542b3cd4f549443eb89128c6d38&features=breezometer_aqi,local_aqi,sources_and_effects,pollutants_aqi_information,health_recommendations'.format(
         str(lat), str(lon))
@@ -17,15 +42,17 @@ def get_air_quality(lat, lon):
             f.write(r.text)
 
     with open(path, 'r') as f:
-        return json.load(f)
+        return parse_json(json.load(f))
 
+
+# parse_json(get_air_quality(48.857456, 2.354611))
 
 # def map_output(lat, lon, radius):
 #     min_lat = lat - radius
 #     max_lat = lat + radius
 #     min_lon = lat - radius
 #     max_lon = lat + radius
-    # folium.folium.Map(location=[lat, lon])
+# folium.folium.Map(location=[lat, lon])
 
 
 # print(json.dumps(get_air_quality(50.857456, 2.354611), indent=2, sort_keys=True))
--- server.py
+++ server.py
@@ -2,27 +2,32 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import envInfo
 import json
 import urllib
+import demographics
 
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         """Respond to a GET request."""
         path = self.path.split('?')[0]
+        if (path == '/favicon.ico'):
+            self.send_response(404)
+            return
         params = urllib.parse.parse_qs(self.path.split('?')[1])
         if path == "/AirQuality":
             self.send_response(200)
             self.send_header("Content-type", "text/json")
             self.end_headers()
-            self.path
-            print(params)
+            addr = params['addr'][0].replace('_', ' ')
+            loc = demographics.addressToCoordinates(addr)
             self.wfile.write(json.dumps(
-                envInfo.get_air_quality(params['lat'][0], params['lon'][0])).encode('utf-8'))
-        if path == "/DemoInfo":
+                envInfo.get_air_quality(loc[0], loc[1])).encode('utf-8'))
+        elif path == "/DemoInfo":
             self.send_response(200)
             self.send_header("Content-type", "text/json")
             self.end_headers()
-            self.path
-            print(params)
+            zip_codes = demographics.zipCodeToZipCodes(params['zip'][0], "1")
+            self.wfile.write(json.dumps(
+                demographics.aggregateAllData(zip_codes)).encode('utf-8'))
 
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
@@ -30,8 +35,5 @@ def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
-
-# GET http://localhost:8000/AirQuality?lon=23.23432&lat=34.23232
-# https://www.programcreek.com/python/example/103649/http.server.BaseHTTPRequestHandler
 
 run(handler_class=MyHandler)
