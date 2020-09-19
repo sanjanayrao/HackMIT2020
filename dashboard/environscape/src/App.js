b4eb9832e6ca12f4b8c28b31b2f92fdf426c75f3
bradh43 <brad.hodkinson2@gmail.com>
2020-09-19 13:29:09 -0500

    removed about

--- dashboard/environscape/src/App.js
+++ dashboard/environscape/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import About from './componets/pages/About/About';
 import Home from './componets/pages/Home/Home';
 
 
@@ -11,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/about" component={About} />
         <Route path='/home' component={Home} />
         <Route path='/environscape' component={Home} />
         <Route path='/' component={Home} />
