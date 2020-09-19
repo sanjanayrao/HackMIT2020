df7ef0ab718e59443b7801ea93977295ec615a16
bradh43 <brad.hodkinson2@gmail.com>
2020-09-19 11:52:28 -0500

    finished demographics script

--- demographics.py
+++ demographics.py
@@ -23,20 +23,11 @@ census_codes = {
 }
 
 def main():
-  pp = pprint.PrettyPrinter(indent=4)
-  # zip_codes = addressToZipCodes("28449 W Harvest Glen Cir", "12")
-  # print(zip_codes)
-  #pp.print(zip_codes)
+  pp = pprint.PrettyPrinter(indent=2)
+  # zip_codes = addressToZipCodes("1036 Heather LN Hartford Wisconsin", "5")
+  zip_codes = zipCodeToZipCodes("53072", "1")
+  pp.pprint(aggregateAllData(zip_codes))
 
-  # TODO make this an env variable. DO NOT PUSH CODE YET!!!!
-  # c = Census("756543c3b2a18464d619945fafe8ef5a6db89267")
-  # test = c.acs5.get(('NAME', 'B25034_010E'), {'for': 'state:{}'.format(states.MD.fips)})
-  # print(test)
-  zipcodes = addressToZipCodes("904 Ash CT Snohomish", "5")
-  pp.pprint(aggregateAllData(zipcodes))
-
-  # ans = getCensusDataDict("60039")
-  # pp.pprint(ans)
 
 # fetches all census data
 def getCensusDataDict(zipcode):
@@ -48,6 +39,7 @@ def getCensusDataDict(zipcode):
 
 # communicates with census API
 def getCensusData(zip_code, census_variable):
+  census_api_key = "756543c3b2a18464d619945fafe8ef5a6db89267"
   url="https://api.census.gov/data/2017/acs/acs5?key="+census_api_key+"&get="+census_variable+"&for=zip%20code%20tabulation%20area:"+zip_code
   payload = ""
   headers = {'content-type': 'application/json'}
@@ -66,20 +58,27 @@ def addressToCoordinates(input_address):
     location = geolocator.geocode(input_address, exactly_one=True)
     return (location.latitude, location.longitude)
 
+
 # return a list of zip codes given an address and a radius surrounding it
-def addressToZipCodes(input_address, radius):
-    geolocator = Nominatim(user_agent="sucky_triangles")
-    location = geolocator.geocode(input_address, exactly_one=True)
-    zip_code_regex = re.compile(r"\s\b(\d{5})(?:-\d{4})?,")
-    zip_code = zip_code_regex.findall(location.address)[0]
-    print(zip_code)
-    url = "https://www.zipcodeapi.com/rest/q9diw5gxk8r7YuGVrwfEmoyfcq0B1asM6Em3yzQs81Cv4EjTTrSToFD5ubo0Jg6V/radius.json/" + zip_code + "/" + radius + "/mile?minimal"
+def zipCodeToZipCodes(zip_code, radius):
+    zip_code_api_key = "vCcE49wpZQzAbQaXsSoUFF13AFX1u9qdqj3DmQ98n9RGKVdleh0Uu85BX6FnlYdp"
+    # zip_code_api_key = "Eg7UvM8pgDDE450RNlrPhbeQoP5eMUsMKSTbm6TYYyj5yYQnB9lCfQAN6PbNIN8p"
+    url = "https://www.zipcodeapi.com/rest/"+zip_code_api_key+"/radius.json/" + zip_code + "/" + radius + "/mile?minimal"
     payload = ""
     headers = {'content-type': 'application/json'}
     response = requests.request("GET", url, data=payload, headers=headers)
     response_dict = json.loads(response.text)
     print(response_dict)
     return response_dict['zip_codes']
+
+
+# return a list of zip codes given an address and a radius surrounding it
+def addressToZipCodes(input_address, radius):
+    geolocator = Nominatim(user_agent="sucky_triangles")
+    location = geolocator.geocode(input_address, exactly_one=True)
+    zip_code_regex = re.compile(r"\s\b(\d{5})(?:-\d{4})?,")
+    zip_code = zip_code_regex.findall(location.address)[0]
+    return zipCodeToZipCodes(zip_code, radius)
 
 
 def aggregateAllData(zip_codes):
