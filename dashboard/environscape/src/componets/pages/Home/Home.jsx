996ae7323a10898689aa0bb0aa60b4dde5fc080b
Matt Wildman <mjwildman15@gmail.com>
2020-09-19 15:43:46 -0500

    Map working!

--- dashboard/environscape/src/componets/pages/Home/Home.jsx
+++ dashboard/environscape/src/componets/pages/Home/Home.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import MapSection from '../../../utility/Map'
 
+const location = {
+    address: 'asdfasdfasdfasdfasdfasdfasdfas dasf asdf asdf asd f.',
+    lat: 37.42216,
+    lng: -122.08427,
+}
 
 class Home extends Component {
     render() {
-        return(
+        return (
             <div>
                 <h1>Home</h1>
+                <MapSection location={location} zoomLevel={17} />
             </div>
         );
     }
 }
 
+
+// // AIzaSyAZJeAuNFC8L1RWxNtq0HCCGTWM43HjFMQ
+// <script
+//    async defer src="https://maps.googleapis.com/maps/api/js?key={YOUR API KEY}
+//    callback=initMap&libraries=drawing"
+//    type="text/javascript">
+// </script>
 export default Home;
---
+++ dashboard/environscape/src/utility/Map.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import './map.css'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import { Icon } from '@material-ui/core'
+
+const Map = ({ location, zoomLevel }) => (
+    <div className="map">
+        <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+        <div className="google-map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyAZJeAuNFC8L1RWxNtq0HCCGTWM43HjFMQ' }}
+                defaultCenter={location}
+                defaultZoom={zoomLevel}
+            >
+                <LocationPin
+                    lat={location.lat}
+                    lng={location.lng}
+                    text={location.address}
+                />
+            </GoogleMapReact>
+        </div>
+    </div>
+)
+
+const LocationPin = ({ text }) => (
+    <div className="pin">
+        <Icon icon={LocationOnIcon} className="pin-icon" />
+        <p className="pin-text">{text}</p>
+    </div>
+)
+
+export default Map
